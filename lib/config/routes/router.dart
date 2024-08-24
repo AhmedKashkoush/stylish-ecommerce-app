f91c2f5764a36a4acccd3f4d49283f3eaa9f113c
Ahmed Kashkoush <89148050+AhmedKashkoush@users.noreply.github.com>
2024-08-24 20:52:10 +0300

    Revert "onBoarding and get started screen"

--- lib/config/routes/router.dart
+++ lib/config/routes/router.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:stylish_ecommerce_app/config/routes/routes.dart';
-import 'package:stylish_ecommerce_app/features/onboarding/view/screens/getting_started_screen.dart';
-
+import 'package:stylish_ecommerce_app/features/onboarding/getting_started_screen.dart';
+import 'package:stylish_ecommerce_app/features/onboarding/onboarding_screen.dart';
 import 'package:stylish_ecommerce_app/features/splash/splash_screen.dart';
-
-import '../../features/onboarding/view/screens/onboarding_screen.dart';
 
 class AppRouter {
   static const String initialRoute = AppRoutes.splash;
@@ -21,7 +19,7 @@ class AppRouter {
         );
       case AppRoutes.onboarding:
         return MaterialPageRoute(
-          builder: (context) => const OnBoardingScreen(),
+          builder: (context) => const OnboardingScreen(),
         );
       default:
         return null;
--- lib/core/constants/images.dart
+++ lib/core/constants/images.dart
@@ -1,8 +1,4 @@
 class AppImages {
   static const String _base = 'assets/images';
   static const String logo = '$_base/logo.png';
-  static const String onBoarding1 = '$_base/on_boarding_1.png';
-  static const String onBoarding2 = '$_base/on_boarding_2.png';
-  static const String onBoarding3 = '$_base/on_boarding_3.png';
-  static const String getStarted = '$_base/get_started.png';
 }
