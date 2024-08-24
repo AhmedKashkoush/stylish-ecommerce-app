92c42487e5d9394b76a47dcf02f5b4363a8771f0
esraaeelbeltagy <esraae.bltagy19@gmail.com>
2024-08-24 20:32:40 +0300

    onBoarding and get started screen

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -1,3 +1,15 @@
 class AppStrings {
   static const String appName = 'Stylish';
-}
+  static const String getStartedTitle = 'You want Authentic, here you go!';
+  static const String getStartedPargraph = 'Find it here, buy it now!';
+  static const String getStarted = 'Get Started';
+  static const String onBoarding1Title = 'Choose Products';
+  static const String onBoarding2Title = 'Make Payment';
+  static const String onBoarding3Title = 'Get Your Order';
+  static const String onBoarding1Pragraph = 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.';
+  static const String onBoarding2Pragraph = 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.';
+  static const String onBoarding3Pragraph = 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.';
+  static const String next = "Next";
+  static const String skip = "Skip";
+  static const String prev = "Prev";
+} 
